88758055001ff18bf57c79d926bb764b06cab39c
gustavoomacedo <guzera96@gmail.com>
2016-08-31 09:53:36 -0300

    Primeiro commit do projeto.

---
+++ FacebookAppTeste/FacebookAppTeste/FacebookAppTeste/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookAppTeste.Views;
+using Xamarin.Forms;
+
+namespace FacebookAppTeste
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage())
+            {
+                Title = "Login no facebook"
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
